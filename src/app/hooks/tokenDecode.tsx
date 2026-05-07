a3674bac0f974281e384e2eba2c19cef7bb65b2a
Guilherme Guerreiro de Oliveira <155542954+GuiGuerreiroo@users.noreply.github.com>
2026-05-07 17:41:56 -0300

    fix: now ci is corect and cd will probably be

--- src/app/hooks/tokenDecode.tsx
+++ src/app/hooks/tokenDecode.tsx
@@ -1,19 +1,25 @@
 import { jwtDecode } from "jwt-decode";
 import type { TokenInterface } from "../interface/tokenInterface";
 
+interface PlainToken {
+    name: string;
+    email: string;
+    "custom:role": string;
+}
+
 export function getDecodedToken(): TokenInterface | null {
     const token = localStorage.getItem('token');
     
     if (!token) return null;
 
     try {
-        const rawData = jwtDecode<any>(token);
+        const rawData = jwtDecode<PlainToken>(token);
         
         return {
             name: rawData.name,
             email: rawData.email,
             role: rawData["custom:role"]
-        };
+        }
     } catch {
         return null;
     }
